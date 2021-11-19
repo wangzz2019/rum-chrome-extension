15eed24f80ff6d1c3e8f6504c8ad7cd82236d789
wangzz2019 <jack.wang@datadoghq.com>
2021-11-20 01:48:43 +0900

    solve the issue that onUpdated is called twice

--- service_worker.js
+++ service_worker.js
@@ -3,27 +3,28 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     // console.log(tab.url);
     if (typeof(tab.url) == "undefined") return;
-    var match_reg;
-    chrome.storage.sync.get({
-        match:'xxx'
-        },function(items){
-        //console.log("appid is:" + items.appid + " and clienttoken is: " + items.clitoken);
-        match_reg=items.match;
-        // console.log(match_reg);
-        if (tab.url.match(match_reg)){
-            //v3
-            chrome.scripting.executeScript({
-                target: {tabId:tab.id},
-                // files: ["content.js","datadog-rum-v3.js"]
-                files: ["datadog-rum-v3.js","content.js"]
-            },() => { });
-            // for v2 source
-            // chrome.tabs.executeScript(tabId,{file:"content.js"},function(){
-            //     chrome.tabs.executeScript(tabId,{file:"datadog-rum-v3.js"});
-            // });
-        }
-    });
-
+    if (changeInfo.status == "complete"){
+        var match_reg;
+        chrome.storage.sync.get({
+            match:'xxx'
+            },function(items){
+            //console.log("appid is:" + items.appid + " and clienttoken is: " + items.clitoken);
+            match_reg=items.match;
+            // console.log(match_reg);
+            if (tab.url.match(match_reg)){
+                //v3
+                chrome.scripting.executeScript({
+                    target: {tabId:tab.id},
+                    // files: ["content.js","datadog-rum-v3.js"]
+                    files: ["datadog-rum-v3.js","content.js"]
+                },() => { });
+                // for v2 source
+                // chrome.tabs.executeScript(tabId,{file:"content.js"},function(){
+                //     chrome.tabs.executeScript(tabId,{file:"datadog-rum-v3.js"});
+                // });
+            }
+        });
+    }
 });
 
 // chrome.tabs.onUpdated.addListener((tab)=>{
